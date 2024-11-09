f7de582bc4f453689b0827e57ce744b5e8f776f6
stargazer <stargazer200811@gmail.com>
2024-11-09 10:39:48 +0900

    while문 예제

--- practice/python 7.py
+++ practice/python 7.py
@@ -10,12 +10,30 @@
 #else:
    # print("c반입니다")
 
-count = int(input("숫자 하나를 입력해주세요"))
+#count = int(input("숫자 하나를 입력해주세요"))
 
-i=0
-stars = ""
-while i < count:
-    stars = stars + "*"
-    i = i + 1
-print(stars)
+#i=0
+#stars = ""
+#while i < count:
+   # stars = stars + "*"
+   # i = i + 1
+#print(stars)
 
+#fibonacci = 1
+#n = 1
+#m = 1
+
+#while fibonacci < 100:
+    #print(fibonacci)
+    #m = n
+    #n = fibonacci
+ #   fibonacci = m + n
+
+fibonacci_list = [1,1]
+n = 1
+
+while fibonacci_list[n-1] + fibonacci_list[n] < 100:
+    fibonacci_list.append(fibonacci_list[n-1] + fibonacci_list[n])
+    n = n+1
+
+   print(fibonacci_list)
