31532d189250e05fe1b75570818f770879a70730
stargazer <stargazer200811@gmail.com>
2025-02-01 11:22:10 +0900

    자판기의 관리자/사용자 모드 정하기

--- practice/python 13.py
+++ practice/python 13.py
@@ -12,3 +12,39 @@
 음료를 추가하고 재고 입력
 유통기한이 지난 음료 처분
 '''
+
+inventory = {'coke':10, 'cider':5, 'milk':5}
+price = {'coke':1600, 'cider':1400, "milk":1500}
+
+
+def print_menu():
+    print("menu를 출력합니다.")
+
+
+def buy_drink():
+    print("음료를 선택하고, 구매합니다.")
+
+
+def switch_mode():
+    print("mode를 전환합니다.")
+
+
+def change_price():
+    print("음료의 가격을 변경합니다.")
+
+
+def add_drink():
+    print("음료를 추가합니다.")
+
+
+def register_drink():
+    print("음료를 등록합니다.")
+
+
+def delete_drink():
+    print("음료를 삭제합니다.")
+
+
+def extract_drink():
+    print("음료를 제거합니다.")
+   
